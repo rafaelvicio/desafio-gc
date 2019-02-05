50824502aa0c58eca0f9a4498e925ee7faf7ebcb
Rafael Augusto <rareis@stefanini.com>
2019-02-05 19:53:15 -0200

    Partidas

--- index.js
+++ index.js
@@ -1,24 +1,85 @@
 const _ = require("lodash")
 
 const times = [
-    {id: '1', name: 'team1'},
-    {id: '2', name: 'team2'},
-    {id: '3', name: 'team3'},
-    {id: '4', name: 'team4'},
-    {id: '5', name: 'team5'},
-    {id: '6', name: 'team6'},
+    {id: '1', name: 'team1', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '2', name: 'team2', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '3', name: 'team3', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '4', name: 'team4', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '5', name: 'team5', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '6', name: 'team6', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '7', name: 'team7', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '8', name: 'team8', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '9', name: 'team9', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '10', name: 'team10', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '11', name: 'team11', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
+    {id: '12', name: 'team12', pontos: 0, vitorias: 0, empates: 0, derrotas: 0},
 ]
+
+console.log("########################")
+console.log("Iniciando campeonato!")
+console.log("########################")
 
 const novoArray = _.shuffle(times)
 
-const grupos = _.chunk(novoArray, 2)
+const grupos = _.chunk(novoArray, 4)
 
-console.log("Os grupos:  ", grupos)
+console.log("########################")
+console.log("Grupos sorteados!")
+console.log("########################")
 
-grupos.map(item => {
-    console.log("O que tem aqui? ", item)
-    
+console.log("Os grupos: ", grupos)
+
+const partidas = [];
+
+console.log("########################")
+console.log("Iniciando as partidas!")
+console.log("########################")
+
+grupos.map(grupo => {
+    grupo.map(time => {
+        grupo.map(adversario => {
+            if(!_.isEqual(time, adversario) && jogouContra(time, adversario)) {
+                partidas.push(jogarPartida(time, adversario))
+            }
+        })
+        
+    })
 })
+
+console.log("As partidas: ", partidas)
+
+console.log("########################")
+console.log("Tabela final de grupos")
+console.log("########################")
+
+console.log("Grupos: ", )
+
+console.log("########################")
+console.log("Vamos dar inicio as playofs")
+console.log("########################")
+
+
+
+
+function jogarPartida(team01, team02) {
+
+    let team01_rounds = _.random(0, 16);
+    let team02_rounds = _.random(0, 16);
+
+    team01_rounds > team02_rounds ? team01_rounds = 16 : team02_rounds = 16
+
+
+
+
+    return {team01: team01.id, team02: team02.id, team01_rounds, team02_rounds }
+}
+
+function jogouContra(time, adversario) {
+    const podeJogar = _.find(partidas, {'team01': time.id, 'team02': adversario.id})
+    const podeJogar2 = _.find(partidas, {'team01': adversario.id, 'team02': time.id})
+    return podeJogar == undefined && podeJogar2 == undefined;
+}
+
 
 
 // Array com 80 times
