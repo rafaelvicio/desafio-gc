2756b90384cf60e38aba630dc3eb572457a468a2
Rafael Augusto <rareis@stefanini.com>
2019-02-08 14:05:11 -0200

    Chegando ate o vencedor

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const _ = require("lodash")
 
+//TODO Forma mais real de reazar o resultado da partida
+//TODO Implementar TBD para grupos impares
+
 const times = [
     {id: '1', name: 'team1', pontos: 0, rounds: 0, derrotas: 0},
     {id: '2', name: 'team2', pontos: 0, rounds: 0, derrotas: 0},
@@ -8,11 +11,7 @@ const times = [
     {id: '5', name: 'team5', pontos: 0, rounds: 0, derrotas: 0},
     {id: '6', name: 'team6', pontos: 0, rounds: 0, derrotas: 0},
     {id: '7', name: 'team7', pontos: 0, rounds: 0, derrotas: 0},
-    {id: '8', name: 'team8', pontos: 0, rounds: 0, derrotas: 0},
-    {id: '9', name: 'team9', pontos: 0, rounds: 0, derrotas: 0},
-    {id: '10', name: 'team10', pontos: 0, rounds: 0, derrotas: 0},
-    {id: '11', name: 'team11', pontos: 0, rounds: 0, derrotas: 0},
-    {id: '12', name: 'team12', pontos: 0, rounds: 0, derrotas: 0},
+    {id: '8', name: 'team8', pontos: 0, rounds: 0, derrotas: 0}
 ]
 
 console.log("########################")
@@ -43,12 +42,10 @@ grupos.map(grupo => {
                 partidas.push(resultadoPartida)
                 console.log("A partida: ", resultadoPartida)
                 if(resultadoPartida.team01_rounds > resultadoPartida.team02_rounds) {
-                    console.log("O time venceu, o adversario perdeu!")
                     time.pontos++
                     time.rounds += resultadoPartida.team01_rounds
                     adversario.derrotas++
                 }else {
-                    console.log("O adversaio venceu!")
                     adversario.pontos++
                     adversario.rounds += resultadoPartida.team02_rounds
                     time.derrotas++
@@ -85,6 +82,9 @@ const timesPlayOffs = [];
 
 gruposComVencedores.map(grupo => {
     grupo.map(time => {
+        time.pontos = 0;
+        time.rounds = 0;
+        time.derrotas = 0;
         timesPlayOffs.push(time)
     })
 }) 
@@ -98,6 +98,79 @@ console.log(timesPlayOffs)
 const timesEmbaralhados = _.shuffle(timesPlayOffs)
 
 console.log("Times embaralhados: ", timesEmbaralhados)
+
+let playoffs = _.chunk(timesEmbaralhados, 2)
+
+console.log("Faze playoofs: ", playoffs)
+
+playoffs.map(partida => {
+    resultadoPartida = jogarPartida(partida[0], partida[1])
+    console.log("Resultado partida: ", resultadoPartida);
+    if(resultadoPartida.team01_rounds > resultadoPartida.team02_rounds) {
+        partida[0].pontos++
+        partida[0].rounds += resultadoPartida.team01_rounds
+        partida[1].derrotas++
+    }else {
+        partida[1].pontos++
+        partida[1].rounds += resultadoPartida.team02_rounds
+        partida[0].derrotas++
+    }  
+})
+
+console.log("Proxima fase", playoffs)
+
+playoffs = playoffs.map(partida => {
+    return _.orderBy(partida, ['pontos', 'rounds'], ['desc', 'desc'])
+}).map(partida => {
+    return _.slice(partida, 0, 1)
+})
+
+finais = [];
+
+console.log("Isso aqui: ", playoffs)
+
+playoffs.map(time => {
+    console.log("O time: ", time)
+    finais.push(time[0])
+} );
+
+console.log("O que vai ser isso", playoffs)
+console.log("Final: ", finais)
+
+//Quando for 2 times, e diferente
+
+    console.log("Partida: ", finais)
+    resultadoPartida = jogarPartida(finais[0], finais[1])
+    console.log("Resultado partida: ", resultadoPartida);
+    if(resultadoPartida.team01_rounds > resultadoPartida.team02_rounds) {
+        finais[0].pontos++
+        finais[0].rounds += resultadoPartida.team01_rounds
+        finais[1].derrotas++
+    }else {
+        finais[1].pontos++
+        finais[1].rounds += resultadoPartida.team02_rounds
+        finais[0].derrotas++
+    }  
+
+console.log("Proxima fase", finais)
+
+finais = playoffs.map(partida => {
+    return _.orderBy(partida, ['pontos', 'rounds'], ['desc', 'desc'])
+}).map(partida => {
+    return _.slice(partida, 0, 1)
+})
+
+console.log("FInal: ", finais)
+
+console.log("Isso aqui: ", playoffs)
+
+vencedor = {}
+
+finais.map(time => {
+    vencedor = time[0];
+} );
+
+console.log("O grande vencedor e: ", vencedor)
 
 function jogarPartida(team01, team02) {
 
@@ -114,8 +187,6 @@ function jogouContra(time, adversario) {
     const podeJogar2 = _.find(partidas, {'team01': adversario.id, 'team02': time.id})
     return podeJogar == undefined && podeJogar2 == undefined;
 }
-
-
 
 // Array com 80 times
 // Dividir em grupos de 5 times
